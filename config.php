d180a532affd61e0ec24c8957bd25212b910f79a
Beycan <bbeycanov@gmail.com>
2017-11-07 16:20:51 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 	*	Author Beycan Beycanov 
 	*/
 	
-	define('URL','https://example.com/');					  	// CHANGE YOUR SITE NAME	
-	define('APP_ID','app-id-here');						  		// CHANGE YOUR FACEBOOK APP ID
-	define('SECRET_KEY','app-secret-here');						// CHANGE YOUR FACEBOOK APP secret key
-	define('GRAPH_VERSIONS','v2.2');							// CHANGE YOUR FACEBOOK API GRAPH VERSIONS
+	define('URL','https://example.com/');   	// CHANGE YOUR SITE NAME	
+	define('APP_ID','app-id-here');		     	// CHANGE YOUR FACEBOOK APP ID
+	define('SECRET_KEY','app-secret-here');		// CHANGE YOUR FACEBOOK APP secret key
+	define('GRAPH_VERSIONS','v2.2');		// CHANGE YOUR FACEBOOK API GRAPH VERSIONS
 ?>
